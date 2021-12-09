047fef81b4c834d1190c9b1c21dc139c91851269
Petar Pribetic <pribetic.petar@gmail.com>
2021-12-09 16:49:00 +0100

    .

--- app.js
+++ app.js
@@ -1,51 +1,53 @@
-const INPUT_VALUE = document.querySelector("#input")
-const SEARCH_BUTTON = document.querySelector("#button")
-const USERDATA = document.querySelector("#userdata")
-const PROFILE = document.querySelector("#profile")
-const AVATAR = document.querySelector("#avatar")
-const USERNAME = document.querySelector("#username")
-const NAME = document.querySelector("#name")
-const BIO = document.querySelector("#bio")
-const REPO = document.querySelector("#repo")
-const ERROR = document.querySelector("#error")
+const INPUT_VALUE = document.querySelector("#input");
+const SEARCH_BUTTON = document.querySelector("#button");
+const USERDATA = document.querySelector("#userdata");
+const PROFILE = document.querySelector("#profile");
+const AVATAR = document.querySelector("#avatar");
+const USERNAME = document.querySelector("#username");
+const NAME = document.querySelector("#name");
+const BIO = document.querySelector("#bio");
+const REPO = document.querySelector("#repo");
+const ERROR = document.querySelector("#error");
 
-const CLIENT_ID = "Iv1.d2df1583b98065bc"
-const CLIENT_SECRET = "d75e07a7dfc3f1c4b7784d3b07955dde57027b1a"
+const CLIENT_ID = "";
+const CLIENT_SECRET = "";
 
 const FETCH_USERS = async (user) => {
-    const API_CALL = await fetch(`https://api.github.com/users/${user}?client_id=${CLIENT_ID}&client_secret=${CLIENT_SECRET}`)
-    const DATA = await API_CALL.json()
-    if (!DATA.id) {
-        ERROR.innerHTML = `<p id="usernotfound">Does not exist</p>`
-        USERDATA.classList.add("hide")
-    } else {
-        ERROR.innerHTML = ""
-        USERDATA.classList.remove("hide")
-        return { data: DATA }
-    }
-}
+  const API_CALL = await fetch(
+    `https://api.github.com/users/${user}?client_id=${CLIENT_ID}&client_secret=${CLIENT_SECRET}`
+  );
+  const DATA = await API_CALL.json();
+  if (!DATA.id) {
+    ERROR.innerHTML = `<p id="usernotfound">Does not exist</p>`;
+    USERDATA.classList.add("hide");
+  } else {
+    ERROR.innerHTML = "";
+    USERDATA.classList.remove("hide");
+    return { data: DATA };
+  }
+};
 
 const SHOW_DATA = () => {
-    FETCH_USERS(INPUT_VALUE.value).then((res) => {
-        AVATAR.src = res.data.avatar_url
-        USERNAME.innerHTML = `@${res.data.login}`
-        NAME.innerHTML = res.data.name
-        BIO.innerHTML = res.data.bio
-        SHOW_REPOS(`${res.data.repos_url}?per_page=100`);
-    })
-}
+  FETCH_USERS(INPUT_VALUE.value).then((res) => {
+    AVATAR.src = res.data.avatar_url;
+    USERNAME.innerHTML = `@${res.data.login}`;
+    NAME.innerHTML = res.data.name;
+    BIO.innerHTML = res.data.bio;
+    SHOW_REPOS(`${res.data.repos_url}?per_page=100`);
+  });
+};
 
 const FETCH_REPOS = async (url) => {
-    const API_CALL = await fetch(url)
-    const DATA = await API_CALL.json()
-    return { data: DATA }
-}
+  const API_CALL = await fetch(url);
+  const DATA = await API_CALL.json();
+  return { data: DATA };
+};
 
 const SHOW_REPOS = (url) => {
-    REPO.innerHTML = ""
-    FETCH_REPOS(url).then((res) => {
-        listItems = res.data.reduce((result, item) => {
-            result += `<li>
+  REPO.innerHTML = "";
+  FETCH_REPOS(url).then((res) => {
+    listItems = res.data.reduce((result, item) => {
+      result += `<li>
                             <div class="repository">
                                 <div>
                                     <h1 class="repository_name">${item.name}</h1>
@@ -61,19 +63,19 @@ const SHOW_REPOS = (url) => {
                                     </div>
                                 </div>
                             </div>
-                        </li>`
-            return result
-        }, '')
-        REPO.innerHTML = listItems
-    })
-}
+                        </li>`;
+      return result;
+    }, "");
+    REPO.innerHTML = listItems;
+  });
+};
 
 INPUT_VALUE.addEventListener("keyup", (event) => {
-    if (event.keyCode === 13) {
-        SHOW_DATA()
-    }
-})
+  if (event.keyCode === 13) {
+    SHOW_DATA();
+  }
+});
 
 SEARCH_BUTTON.addEventListener("click", () => {
-    SHOW_DATA()
-})
+  SHOW_DATA();
+});
